3010c8280ea82e1b14c36e13f94d824a73ba6393
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2022-04-14 17:02:14 -0300

    Update App.tsx

--- Typeform/src/App.tsx
+++ Typeform/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Main, Section, Sticky } from './styles'
-
+//importações que serão utilizadas
 import GlobalStyles from './styles/global'
 import Header from './components/Header'
 import FirstAndSecond from './components/FirstAndSecond'
@@ -14,20 +14,21 @@ function App() {
 
   return (
     <>
-      <GlobalStyles />
+      <GlobalStyles /> // variavel que vai conter os nossos CSS
 
-      <Main style={{ height: '1610vh' }}>
+      //main vai conter todos os containers animados da nossa página
+      <Main style={{ height: '1610vh' }}>// 1610vh será a o tamanho da nossa página
         <Header />
 
-        <Section style={{ height: '72.7%' }}>
+        <Section style={{ height: '72.7%' }}>//Primeira e segunda section animada 
           <FirstAndSecond />
         </Section>
 
-        <Section style={{ height: '9.7%' }}>
+        <Section style={{ height: '9.7%' }}> // Terceira section animada
           <Sticky className="third" />
         </Section>
 
-        <Section style={{ height: '10.1%' }}>
+        <Section style={{ height: '10.1%' }}> // Quarta section animada
           <Sticky className="fourth" />
         </Section>
 
