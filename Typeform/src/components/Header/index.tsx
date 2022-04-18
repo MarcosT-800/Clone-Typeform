81d5ed0963bc160b419636e4e306192d02a15673
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2022-04-18 15:28:40 -0300

    Update index.tsx

--- Typeform/src/components/Header/index.tsx
+++ Typeform/src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { Container } from './styles'
 
 const Header: React.FC = () => {
   const { scrollYProgress } = useViewportScroll()
-  const y = useTransform(scrollYProgress, [0.188, 0.198], ['0%', '-100%'])
+  const y = useTransform(scrollYProgress, [0.188, 0.198], ['0%', '-100%'])//header irá subir quando scrolla
 
   return <Container style={{ y }} />
 }
